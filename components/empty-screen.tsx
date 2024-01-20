227c525489c8d2637ad0a50e6c11dc2669566e2f
Veerbal Singh <Veerbalsingh1@gmail.com>
2024-01-20 15:43:14 +0530

    Updated hero text

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,29 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Decode Tech Jargon',
+    message: 'What does "asynchronous programming" mean?'
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Enhance Codebase',
+    message: 'How can I refactor this block of code for better readability?'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Understand Best Practices',
+    message: 'Why is test-driven development considered effective?'
+  },
+  {
+    heading: 'Optimize Application',
+    message: 'What strategies can I use to improve database query performance?'
+  },
+  {
+    heading: 'Troubleshoot Errors',
+    message: 'How do I fix a "memory leak" in my application?'
+  },
+  {
+    heading: 'Implement Features',
+    message:
+      'Can you guide me through setting up a CI/CD pipeline for my project?'
   }
 ]
 
@@ -24,15 +37,15 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to Veerbal Chatbot
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          Your agile Coding Assistant! Specializing in code optimization,
+          refactoring, and testing, Veerbal ensures your web applications are
+          scalable, maintainable, and of top quality. Embrace best practices,
+          TDD, and efficient project management with Veerbal's expertise in
+          Agile methodologies, Git, and CI/CD pipelines. Start coding smarter
+          today!
         </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
